bfbeaf41075880e8c1aed940b1e6e99b01a56a12
Luca Bruzzone <spam@unsigned.it>
2015-05-21 10:49:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/unsigned/provamaterial/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.unsigned.provamaterial;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    private MenuDrawer mDrawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mDrawerFragment = (MenuDrawer) getFragmentManager().findFragmentById(R.id.fragment_navigation_bar_impl);
+        mDrawerFragment.setUp(R.id.fragment_navigation_bar_impl, (DrawerLayout) findViewById(R.id.main_drawer), mToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/unsigned/provamaterial/MenuDrawer.java
@@ -0,0 +1,118 @@
+package com.example.unsigned.provamaterial;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuDrawer extends Fragment {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstance;
+
+    private static final String PREF_FILE_NAME = "com.unsigned.provamaterial";
+    private static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+
+    private View mContainerView;
+
+    public MenuDrawer(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUserLearnedDrawer =  Boolean.getBoolean(readToPreference(getActivity(), KEY_USER_LEARNED_DRAWER, "false"));
+
+        if (savedInstanceState != null) {
+            mFromSavedInstance = true;
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu_drawer, container, false);
+
+    }
+
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+
+        this.mContainerView = getActivity().findViewById(fragmentId);
+        this.mToolbar = toolbar;
+
+        this.mDrawerLayout = drawerLayout;
+
+        // seetup the ActionBarToggle
+        this.mDrawerToggle = new ActionBarDrawerToggle(
+                getActivity(),
+                mDrawerLayout,
+                mToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                /*
+                if(!mUserLearnedDrawer){
+                    mUserLearnedDrawer = true;
+                    saveToPreference(getActivity(), KEY_USER_LEARNED_DRAWER, String.valueOf(mUserLearnedDrawer));
+                }
+                */
+                // redraw the actionbar
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+        };
+
+        // if the user has returned from saved instance and
+        // the drawer was previously opened we reopened it
+
+        /*
+        if(!mUserLearnedDrawer && !mFromSavedInstance) {
+            mDrawerLayout.openDrawer(mContainerView);
+
+        }
+        */
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        // we need do tell the drawerActivity that we need to redraw
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+
+    }
+
+    public static void saveToPreference (Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+    public static String readToPreference (Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+}
